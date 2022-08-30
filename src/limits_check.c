553454a7bb010138c13aaf309be5196e42cee496
Elio <alondot@gmail.com>
2022-08-30 12:11:29 +0200

    map validation

---
+++ src/limits_check.c
@@ -0,0 +1,45 @@
+#include "so_long.h"
+
+void	ft_limits_error(void)
+{
+	ft_printf("Error!\nNot enough wall in the screen limits.\n");
+	exit(1);
+}
+
+void	ft_check_x_limits(t_game *game)
+{
+	while ((game->x) < (game->map_x))
+	{
+		if (game->map[0][game->x] != '1')
+			ft_limits_error();
+		game->x++;
+	}
+	game->y = game->map_y - 1;
+	game->x = 0;
+	while ((game->x) < (game->map_x))
+	{
+		if (game->map[game->y][game->x] != '1')
+			ft_limits_error();
+		game->x++;
+	}
+}
+
+void	ft_check_y_limits(t_game *game)
+{
+	game->x = 0;
+	game->y = 0;
+	while ((game->y) < (game->map_y))
+	{
+		if (game->map[game->y][0] != '1')
+			ft_limits_error();
+		game->y++;
+	}
+	game->y = 0;
+	game->x = game->map_x - 1;
+	while ((game->y) < (game->map_y))
+	{
+		if (game->map[game->y][game->x] != '1')
+			ft_limits_error();
+		game->y++;
+	}
+}
